94900e0110332c6debc20b59e49284580cfc4e28
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-11-27 04:15:17 +0900

    MenuTabDTO completed

--- src/kr/ac/kyonggi/gabrielyoon7/handler/dto/main/MenuTabDTO.java
+++ src/kr/ac/kyonggi/gabrielyoon7/handler/dto/main/MenuTabDTO.java
@@ -1,4 +1,21 @@
 package kr.ac.kyonggi.gabrielyoon7.handler.dto.main;
 
 public class MenuTabDTO {
+    public String tab_id,tab_title;
+
+    public String getTab_id() {
+        return tab_id;
+    }
+
+    public void setTab_id(String tab_id) {
+        this.tab_id = tab_id;
+    }
+
+    public String getTab_title() {
+        return tab_title;
+    }
+
+    public void setTab_title(String tab_title) {
+        this.tab_title = tab_title;
+    }
 }
